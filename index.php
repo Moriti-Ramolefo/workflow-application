d2f2b11c9a7f435607ab82f6b0936dc223117a6b
Moriti-Ramolefo <ramolefo.moriti@gmail.com>
2022-11-15 21:20:08 +0200

    custom commits

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 	<title>Admin/User Login</title>
 	<link rel="icon" href="assets/img/icon.jpg">
 	<link rel="stylesheet" href="assets/css/loading.css">
+	<link rel="stylesheet" href="assets/css/glitch.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap" rel="stylesheet">
 </head>
 <body>
 
@@ -15,22 +19,9 @@
 		background: linear-gradient(#00b16a, rgba(0, 0, 0, 0.5), #33b5e5), url(assets/img/banner.jpg) !important;
 		background-size: cover !important;
 		backdrop-filter: blur(5px) !important;
-	}
-
-	.iframe-container{
 		position: relative;
-		width: 100%;
-		padding-bottom: 56.25%; 
-		height: 0;
 	}
-	.iframe-container iframe{
-		position: absolute;
-		top:0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-	}
-	</style>
+</style>
 
 <?php
 require 'authentication.php';
@@ -57,7 +48,18 @@ include("includes/login_header.php");
 
 <!-- Login platform -->
 
+<div class="container">
+  	<div class="glitch" data-text="WATCH THIS VIDEO BEFORE USING THE APP">WATCH THIS VIDEO BEFORE USING THE APP</div> 
+  		<div class="glow">WATCH THIS VIDEO BEFORE USING THE APP</div>
+  			<p class="subtitle"></p>
+</div>
+<div class="scanlines"></div>
 <div class="row">
+	<div class="col-md-4">
+		<div style="position:relative;top:20vh;">
+			<iframe class="embed-responsive-item" width="760" height="440" src="https://www.youtube.com/embed/I5SIc7hVEl0" allowfullscreen></iframe>
+		</div>
+	</div>
 	<div class="col-md-4 col-md-offset-3">
 		<div class="well" style="position:relative;top:20vh;">
 			<form class="form-horizontal form-custom-login" action="" method="POST">
@@ -80,6 +82,7 @@ include("includes/login_header.php");
 		</div>
 	</div>
 </div>
+
 
 <?php
 
