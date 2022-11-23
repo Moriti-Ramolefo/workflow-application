69bd1f80720deedced8e7805fcae2109f71cc185
Moriti-Ramolefo <ramolefo.moriti@gmail.com>
2022-11-23 08:34:45 +0200

    final commit for real

--- config.php
+++ config.php
@@ -9,7 +9,8 @@ define('DB_NAME', 'drive_files');
 define('GOOGLE_CLIENT_ID', '157917776823-a58mlq6h9a068nup6lelorc3a4d33jfb.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
 define('GOOGLE_OAUTH_SCOPE', 'https://www.googleapis.com/auth/drive');
-define('REDIRECT_URI', 'http://localhost:8888/workflow-application/gd-sync.php');
+define('REDIRECT_URI', 'http://workflow-application.herokuapp.com/gd-sync.php');
+// define('REDIRECT_URI', 'http://localhost:8888/workflow-application/gd-sync.php');
 
 // Start session
 if(!session_id()) session_start();
